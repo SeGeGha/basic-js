40c2eb95d7e2b5af4cff3af3ed5adc8350b36ed0
Siarhei Sudakov <s3gaddtv@gmail.com>
2020-02-18 12:24:50 +0300

    feat: add carbon-dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,8 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+    let valueActivity = (typeof(sampleActivity) == 'string') ? Number.parseFloat(sampleActivity) : false;
+    if (!valueActivity || valueActivity > 15 || valueActivity < 0) return false;
+    return Math.ceil(Math.log(MODERN_ACTIVITY / valueActivity) * HALF_LIFE_PERIOD / 0.693);
 };
