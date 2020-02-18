0c6b646491195774fb7a9210a54938b75e372914
Siarhei Sudakov <s3gaddtv@gmail.com>
2020-02-18 12:26:08 +0300

    refactor: change structure carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,7 +2,9 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-    let valueActivity = (typeof(sampleActivity) == 'string') ? Number.parseFloat(sampleActivity) : false;
-    if (!valueActivity || valueActivity > 15 || valueActivity < 0) return false;
-    return Math.ceil(Math.log(MODERN_ACTIVITY / valueActivity) * HALF_LIFE_PERIOD / 0.693);
+  let valueActivity = (typeof(sampleActivity) == 'string') ? Number.parseFloat(sampleActivity) : false;
+  
+  if (!valueActivity || valueActivity > 15 || valueActivity < 0) return false;
+  
+  return Math.ceil(Math.log(MODERN_ACTIVITY / valueActivity) * HALF_LIFE_PERIOD / 0.693);
 };
