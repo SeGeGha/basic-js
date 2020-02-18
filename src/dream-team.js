0360d500e8ed2d8957bc10cf922ba2818252713d
Siarhei Sudakov <s3gaddtv@gmail.com>
2020-02-18 15:47:35 +0300

    feat: add dream-team solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,17 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(dreamTeamArray) {
+  if (Array.isArray(dreamTeamArray)) {
+    let nameDreamTeam = [];
+        
+    dreamTeamArray = dreamTeamArray.filter( value => typeof(value) == 'string')
+                                   .map(name => name.replace(/\s+/g, ''));
+    
+    for (let i = 0; i < dreamTeamArray.length; i++) {
+      nameDreamTeam.push(dreamTeamArray[i].slice(0, 1)
+                                          .toUpperCase());
+    }
+
+    return nameDreamTeam.sort().join(''); 
+  } else {
+    return false;
+  }
 };
