6be5c502c1c923bdc8c6431e0e62deeca4754088
Siarhei Sudakov <s3gaddtv@gmail.com>
2020-02-19 17:00:47 +0300

    feat: add extended-repeater solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,36 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let {
+    repeatTimes,
+    separator,
+    addition,
+    additionRepeatTimes,
+    additionSeparator
+  } = options;
+
+  str = String(str);
+  repeatTimes = isUndefined(repeatTimes, 1);
+  separator = isUndefined(separator, '+');
+  additionRepeatTimes = isUndefined(additionRepeatTimes, 1);
+  additionSeparator = isUndefined(additionSeparator, '|');
+
+  if (addition !== undefined) {
+    addition = String(addition);
+    str += repeatSubStr(addition,additionRepeatTimes,additionSeparator);
+  }
+
+  return repeatSubStr(str,repeatTimes,separator);
+
+  function isUndefined(name, value) {
+    return name = name === undefined ? value: name;
+  }  
+
+  function repeatSubStr(substr, numberRepeat, strSeparator) {
+    let arr = []
+            
+    for(let i = 1; i <= numberRepeat; i++) {
+      arr.push(substr) ;
+    }
+
+    return arr.join(strSeparator);  
+  }
 };
-  
