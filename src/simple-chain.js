25755e3557a0ddec32b4297188d2450a41af6833
Siarhei Sudakov <s3gaddtv@gmail.com>
2020-02-19 12:59:41 +0300

    feat: add chain-maker solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,29 @@
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+      return this.chain.length;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    (value !== undefined) ? this.chain.push(`( ${value} )`) : this.chain.push(`( )`);
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+     if (typeof(position) != 'number' || position >= this.chain.length || position <= 0) {
+         this.chain = [];
+         throw Error();
+     } else {
+         this.chain.splice(--position, 1);
+         return this;
+     }
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+      this.chain.reverse();
+      return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+      const result = this.chain.join('~~');
+      this.chain = [];
+      return result;
   }
 };
 
